7092f71b46a2dace486f1c83ab6cabc8ff611375
mhaln3mi <maan.neami@gmail.com>
2022-08-18 10:05:17 +0300

    new data

---
+++ data/ytAPIComments.py
@@ -0,0 +1,67 @@
+from lib2to3.pgen2 import token
+import os
+from urllib import response
+import pandas as pd 
+
+from googleapiclient.discovery import build
+youtube = build('youtube', 'v3', developerKey='AIzaSyArAXXQzyo8XBs5o5lm5aKL8_aRB_YDG1M')
+
+
+
+box = [['Name', 'Comments', 'Time', 'Likes', 'Reply Count']]
+
+#Eudereka, Programming with Mosh, freeCodeCamp
+code_lang = [
+    {"id":"BpPJxtOk8uw"}
+]
+
+# sql_vids = pd.DataFrame([])
+
+
+for id_code in code_lang:
+    def scrape_comments_with_replies():
+        data = youtube.commentThreads().list(part='snippet', videoId=id_code['id'], maxResults='100', textFormat="plainText").execute()
+        
+        for i in data["items"]:
+            name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
+            comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
+            published_at = i["snippet"]['topLevelComment']["snippet"]['publishedAt']
+            likes = i["snippet"]['topLevelComment']["snippet"]['likeCount']
+            replies = i["snippet"]['totalReplyCount']
+            
+            box.append([name, comment, published_at, likes, replies])
+            
+            totalReplyCount = i["snippet"]['totalReplyCount']
+            
+
+        while ("nextPageToken" in data):
+            
+            data = youtube.commentThreads().list(part='snippet', videoId=id_code['id'], pageToken=data["nextPageToken"],
+                                             maxResults='100', textFormat="plainText").execute()
+                                             
+            for i in data["items"]:
+                name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
+                comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
+                published_at = i["snippet"]['topLevelComment']["snippet"]['publishedAt']
+                likes = i["snippet"]['topLevelComment']["snippet"]['likeCount']
+                replies = i["snippet"]['totalReplyCount']
+
+                box.append([name, comment, published_at, likes, replies])
+
+                totalReplyCount = i["snippet"]['totalReplyCount']
+
+                
+
+        df = pd.DataFrame({'Name': [i[0] for i in box], 'Comments': [i[1] for i in box], 'Time': [i[2] for i in box],
+                       'Likes': [i[3] for i in box], 'Reply Count': [i[4] for i in box]})
+        
+        sql_vids = pd.DataFrame([])
+
+        sql_vids = sql_vids.append(df, ignore_index = True)
+
+        sql_vids.to_csv(r'C:\Users\wled3\ytdf17.csv', index=False, header=False)
+    
+    scrape_comments_with_replies()
+
+
+
